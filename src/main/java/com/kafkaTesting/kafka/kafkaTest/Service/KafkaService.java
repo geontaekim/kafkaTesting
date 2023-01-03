ed66e81a561da09c4339bc3d1eaf41239253f9bb
geontaekim <109645318+geontaekim@users.noreply.github.com>
2023-01-03 13:43:58 +0900

    sadsa
    sadsad

--- src/main/java/com/kafkaTesting/kafka/kafkaTest/Service/KafkaService.java
+++ src/main/java/com/kafkaTesting/kafka/kafkaTest/Service/KafkaService.java
@@ -1,9 +1,12 @@
 package com.kafkaTesting.kafka.kafkaTest.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.kafkaTesting.kafka.kafkaTest.mapper.KafkaMapper;
@@ -17,8 +20,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class KafkaService {
 	
+
+	
 	@Autowired
 	private KafkaMapper kafkamapper;
+	
+	private final KafkaTemplate<String, String> kafkaTemplate;
 
 	
 	public ArrayList<HashMap<String , Object>> selectEmpList(HashMap<String,Object> map){
@@ -28,9 +35,24 @@ public class KafkaService {
 		
 		log.debug("[checkResult]"+result);
 		
+		this.kafkaTemplate.send("guntae",result.toString());
+	
+		
 		return result;
 		
 	} 
 	
+	 @KafkaListener(topics = "guntae", groupId = "group-id-oing")
+	    public void consume(String message) throws IOException {
+		 	log.debug("csadsadsadsadsadsadsad");
+		 	
+		 	
+		 	
+	        log.debug("check:::::"+message);
+	        
+	        
+	        
+	        
+	    }
 	
 }
